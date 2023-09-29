96a950b4d3de790bba67b4ee5f5a5c800c922eb5
AASTHA GUPTA <100026195+aastha0203@users.noreply.github.com>
2023-09-29 19:08:14 +0530

    user screen made

---
+++ app/src/main/java/com/example/userscreen/MainActivity.kt
@@ -0,0 +1,203 @@
+package com.example.userscreen
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.userscreen.ui.theme.UserScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UserScreenTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ProfileScreen()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen(){
+    val notification= rememberSaveable{mutableStateOf("")}
+    if (notification.value.isNotEmpty()){
+        Toast.makeText(LocalContext.current,notification.value, Toast.LENGTH_LONG).show()
+        notification.value=""
+    }
+    var name by rememberSaveable {
+        mutableStateOf("default name")}
+    var about by rememberSaveable {
+        mutableStateOf("default about")
+    }
+    var username by rememberSaveable {
+        mutableStateOf("default user_name")
+    }
+
+
+    Column(modifier= Modifier
+        .verticalScroll(rememberScrollState())
+        .padding(8.dp)) {
+    Row(modifier= Modifier
+        .fillMaxSize()
+        .padding(8.dp)
+        //.background(Color.White)
+        , horizontalArrangement = Arrangement.SpaceBetween){
+        Text(text = "Cancel",modifier=Modifier.clickable { notification.value="No changes Done" })
+        Text(text = "Save",modifier=Modifier.clickable { notification.value="Changes Saved" })
+
+    }
+    ProfileImage()
+        Spacer(modifier = Modifier.height(45.dp))
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(text = "Name",modifier=Modifier.width(100.dp))
+            TextField(value = name, onValueChange ={name=it},
+
+                colors=TextFieldDefaults.textFieldColors(
+                    textColor = Color.Black
+                ))
+
+
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+        Row(
+            modifier= Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(
+                text="About",modifier= Modifier
+                    .width(100.dp)
+                    .padding(top = 8.dp)
+            )
+            TextField(value = about, onValueChange = {about=it
+            }, colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.Black
+            ),
+                singleLine = false,
+                modifier = Modifier.height(50.dp))
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(text = "Username",modifier=Modifier.width(100.dp))
+            TextField(value = username, onValueChange ={username=it},
+                colors=TextFieldDefaults.textFieldColors(
+                    textColor = Color.Black
+                ))
+
+        }
+
+
+
+
+}
+
+}
+@Composable
+fun ProfileImage() {
+    val imageUri = rememberSaveable { mutableStateOf("") }
+    val painter= rememberAsyncImagePainter(
+        if (imageUri.value.isEmpty()){
+            R.drawable.baseline_person_24
+        }
+        else{
+            imageUri.value
+        }
+    )
+    val launcher= rememberLauncherForActivityResult(contract =ActivityResultContracts.GetContent()){uri: Uri?->
+        uri?.let { imageUri.value=it.toString() }
+
+    }
+
+
+    Column (
+        modifier= Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Card(shape= CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(100.dp)) {
+            Image(painter = painter, contentDescription = null,modifier= Modifier
+                .wrapContentSize()
+                .clickable { launcher.launch("image/") },
+                contentScale = ContentScale.Crop)
+
+        }
+        Text(text = "Change profile picture")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewOfProfileScreen(){
+    ProfileScreen()
+}
+
+
+
+
+
+
